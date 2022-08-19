6b8a0ab028d5f611e27cfd06db576c600c64546e
Steve <wenzelstev@gmail.com>
2022-08-19 11:45:46 -0400

    experimenting with button animations

---
+++ Assets/Resources/Scripts/UI/ModalControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModalControl : MonoBehaviour
+{
+    public void OnClickModal()
+    {
+        Destroy(gameObject);
+    }
+}
