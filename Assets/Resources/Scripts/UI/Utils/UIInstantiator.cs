e77641cca698c0500631b16240e23df53aeb8b5c
wenzstev <steve.wenzel@datadoghq.com>
2022-08-27 20:00:00 -0400

    work on changing the file structure for the project

---
+++ Assets/Resources/Scripts/UI/Utils/UIInstantiator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInstantiator : MonoBehaviour
+{
+    public void CreateObj(GameObject obj)
+    {
+        GameObject canvas = FindObjectOfType<Canvas>().gameObject;
+        GameObject objInstance = Instantiate(obj, canvas.transform);
+    }
+}
