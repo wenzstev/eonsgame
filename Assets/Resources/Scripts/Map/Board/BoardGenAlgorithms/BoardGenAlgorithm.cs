0708146637a8918f8c83e64cc4d8492b12fad38b
Steve <wenzelstev@gmail.com>
2022-09-13 10:12:58 -0400

    working water level addition

---
+++ Assets/Resources/Scripts/Map/Board/BoardGenAlgorithms/BoardGenAlgorithm.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BoardGenAlgorithm : MonoBehaviour
+{
+    public abstract int[,] getLevelledBoard(int numLevels, int boardWidth, int boardHeight);
+
+    protected float[,] createPerlinBoard(float scale, int sampleNumX, int sampleNumY)
+    {
+
+        Vector2 startPosition = new Vector2(Random.value * 10, Random.value * 10);
+        float[,] points = new float[sampleNumX, sampleNumY];
+        for (int i = 0; i < sampleNumY; i++)
+        {
+            for (int j = 0; j < sampleNumX; j++)
+            {
+                float currentSampleX = Mathf.Lerp(startPosition.x, startPosition.x + scale, Mathf.InverseLerp(0, sampleNumX, j));
+                float currentSampleY = Mathf.Lerp(startPosition.y, startPosition.y + scale, Mathf.InverseLerp(0, sampleNumY, i));
+                points[j, i] = Mathf.PerlinNoise(currentSampleX, currentSampleY);
+            }
+        }
+
+        return points;
+    }
+
+}
--- Assets/Resources/Scripts/Map/Board/IBoardGenAlgorithm.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BoardGenAlgorithm : MonoBehaviour
-{
-    public abstract int[,] getLevelledBoard(int numLevels, int boardWidth, int boardHeight);
-}
