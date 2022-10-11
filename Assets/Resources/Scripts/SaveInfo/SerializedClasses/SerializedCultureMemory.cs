b640d52bbdc6217e2d8fdb8fd8004cc04e224d70
Steve <wenzelstev@gmail.com>
2022-10-11 10:09:35 -0400

    move to new files for easier future work

---
+++ Assets/Resources/Scripts/SaveInfo/SerializedClasses/SerializedCultureMemory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class SerializedCultureMemory
+{
+    public int previousTile;
+    public int previousState;
+    public string cultureParentName;
+    public bool wasRepelled;
+
+    public SerializedCultureMemory(CultureMemory cm)
+    {
+        previousTile = cm.previousTile.id;
+        previousState = (int)cm.previousState;
+        cultureParentName = cm.cultureParentName;
+        wasRepelled = cm.wasRepelled;
+    }
+}
