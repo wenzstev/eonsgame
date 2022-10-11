53718833644226edf488630e969a3c729b4515b8
Steve <wenzelstev@gmail.com>
2022-10-11 10:26:26 -0400

    moved board business logic out of save file

--- Assets/Resources/Scripts/SaveInfo/SerializedClasses/SerializedBoard.cs
+++ Assets/Resources/Scripts/SaveInfo/SerializedClasses/SerializedBoard.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-public class NewBehaviourScript1 : MonoBehaviour
+
+public class SerializedBoard 
 {
+    public List<SerializedTile> tiles;
+    public int width;
+    public int height;
 
-    // Use this for initialization
-    void Start()
+
+    public SerializedBoard(Board b)
     {
+        width = b.Width;
+        height = b.Height;
 
-    }
+        tiles = new List<SerializedTile>();
 
-    // Update is called once per frame
-    void Update()
-    {
-
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                tiles.Add(new SerializedTile(b.tiles.GetTile(x, y), x, y));
+            }
+        }
     }
 }
