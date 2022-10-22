51a3e4cd8276b633cd2ae29e638a7107df52724b
Steve <wenzelstev@gmail.com>
2022-10-22 12:01:07 -0400

    separate files for board, tiles, and cultures

---
+++ Assets/Resources/Scripts/SaveInfo/SerializedGameObjects/SerializedCultures.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SerializedCultures
+{
+    public List<SerializedCultures> cultures;
+
+    public SerializedCultures(Board b)
+    {
+
+    }
+}
