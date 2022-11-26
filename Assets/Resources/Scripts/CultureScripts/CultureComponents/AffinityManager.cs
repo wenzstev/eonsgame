81db748d8475c69f28432a2deaf93eb6e8272fda
wenzstev <steve.wenzel@datadoghq.com>
2022-11-26 18:04:05 -0500

    working affinity system

--- Assets/Resources/Scripts/CultureScripts/CultureComponents/AffinityManager.cs
+++ Assets/Resources/Scripts/CultureScripts/CultureComponents/AffinityManager.cs
@@ -6,27 +6,33 @@ using System.Linq;
 
 public class AffinityManager : MonoBehaviour
 {
+    TileDrawer.BiomeType _currentBiome;
+
     Dictionary<TileDrawer.BiomeType, float> affinities;
+    Dictionary<TileDrawer.BiomeType, DecayTracker> decayRates;
+    Dictionary<TileDrawer.BiomeType, int> daysHarvested;
     GompertzCurve AffinityGrowthRate;
-    ExponentialCurve AffinityDecayRate;
 
     public float Asymptote = -1f;
     public float XDisplacement = 2;
     public float GrowthRate = .5f;
     public float YDisplacement = 1f;
-
-    public float DecaySteepness = .1f;
+    
 
 
     public void Initialize()
     {
         affinities = new Dictionary<TileDrawer.BiomeType, float>();
+        decayRates = new Dictionary<TileDrawer.BiomeType, DecayTracker>();
+        daysHarvested = new Dictionary<TileDrawer.BiomeType, int>();
         foreach (TileDrawer.BiomeType biome in Enum.GetValues(typeof(TileDrawer.BiomeType)))
         {
-            affinities.Add(biome, 1f); // cultures are bad at gathering food initially
+            affinities.Add(biome, 0); // cultures are bad at gathering food initially
+            daysHarvested.Add(biome, 0);
         }
         AffinityGrowthRate = new GompertzCurve(Asymptote, XDisplacement, GrowthRate, YDisplacement);
-        AffinityDecayRate = new ExponentialCurve(DecaySteepness);
+        _currentBiome = TileDrawer.BiomeType.Barren;
+        EventManager.StartListening("Tick", OnTick);
     }
 
     public float GetAffinity(TileDrawer.BiomeType biome)
@@ -42,25 +48,92 @@ public class AffinityManager : MonoBehaviour
 
     public void HarvestedOnBiome(TileDrawer.BiomeType biome)
     {
+        daysHarvested[biome] += 1;
+        //Debug.Log($"Days harvested for biome is {daysHarvested[biome]}");
+
+        if(_currentBiome != biome) 
+        {
+            // create decay curve for current affinity
+            AdjustNewAffinityForDecay(biome);
+
+        }
+        _currentBiome = biome;
+        IncrementAffinity(biome);
+    }
+
+    void AdjustNewAffinityForDecay(TileDrawer.BiomeType biome)
+    {
+            DecayTracker newDecayTracker = new DecayTracker(_currentBiome, daysHarvested[_currentBiome]);
+            decayRates.Add(_currentBiome, newDecayTracker);
+
+            if(decayRates.ContainsKey(biome))
+            {
+                float decayPercent = decayRates[biome].GetDecayRate();
+                Debug.Log($"decay rate for {biome} is {decayPercent}");
+                ChangeAffinity(biome, affinities[biome] * decayPercent);
+                decayRates.Remove(biome);
+            }
+    }
+
+    public void IncrementAffinity(TileDrawer.BiomeType biome)
+    {
         float currentAffinity = GetAffinity(biome);
         float valToAdd = AffinityGrowthRate.GetPointOnCurve(currentAffinity);
         ChangeAffinity(biome, currentAffinity + valToAdd);
     }
 
-    public void DecayAffinities(TileDrawer.BiomeType biome)
+    public void OnTick(Dictionary<string, object> empty)
     {
-        foreach(TileDrawer.BiomeType curBiome in Enum.GetValues(typeof(TileDrawer.BiomeType)))
+        foreach(KeyValuePair<TileDrawer.BiomeType, DecayTracker> kvp in decayRates)
         {
-            if(curBiome != biome)
-            {
-                float currentAffinity = GetAffinity(curBiome);
-                float valToSubtract = AffinityDecayRate.GetPointOnCurve(currentAffinity) -1;
-                ChangeAffinity(curBiome, currentAffinity - valToSubtract);
-            }
+            if(kvp.Key != _currentBiome) kvp.Value.IncreaseDaysSinceHarvested(); // all biomes that aren't the current one decay a bit
         }
     }
 
+    class DecayTracker
+    {
+        public TileDrawer.BiomeType Biome { get; private set; }
+        public int DaysSinceHarvested { get; private set;}
+        PowerCurve _forgettingCurve;
+        public float CurrentDecayRate { get; private set; } // the rate at which knowledge is lost
+
+        public DecayTracker(TileDrawer.BiomeType biome, float NumDaysHarvested)
+        {
+            DaysSinceHarvested = 0;
+            CurrentDecayRate = -.5f; 
+            _forgettingCurve = new PowerCurve(2, 0, 100, -1/NumDaysHarvested);
+            Debug.Log($"forgetting curve for {biome} is {_forgettingCurve.PowerMultiplier}");
+        }
+
+        public float GetDecayRate()
+        {
+            return _forgettingCurve.GetPointOnCurve(DaysSinceHarvested) * .01f;
+        }
+
+        public void IncreaseDaysSinceHarvested() // TODO: some sort of easy way to get the current date so that this is unnecessary
+        {
+            DaysSinceHarvested += 1;
+        }
+    }
+
+
     /*
+
+    maybe decay happens as a function of the amount of time since being on a tile?
+
+    struct Decay
+       Biome biome;
+       int daysSinceHarvested;
+       ExponentialCurve forgettingCurve
+       float lastAffinityLevel
+
+       memory decays exponentially; the curve is flattened the more that a culture has been on a tile
+       don't calculate decay until a culture is ready to harvest from biome again
+
+
+
+
+
      void HarvestedOnBiome(biome b)
         use some curve to calculate the next 'step' up the affinity level
         figure out the increase in affinity and add it to the value
@@ -83,6 +156,18 @@ public class AffinityManager : MonoBehaviour
     3.038 -> 3.038 - .35
     2.688
      
+
+
+    1. track current biome
+    2. when harvesting:
+        - on same biome -> increase affinity, add day to harvest day tracker
+        - on new biome -> create decay tracker for previous biome, decay curve based on number of harvest days
+            - check for existing decay curve on biome, return result as intial affinity level
+            - destroy existing decay curve
+
+
+
+
      */
 
     // decay affinity? 
--- Assets/Resources/Scripts/Tests/CultureAffinityTestSuite.cs
+++ Assets/Resources/Scripts/Tests/CultureAffinityTestSuite.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 using System.Linq;
 
 
-public class CultureAffinityTestSuite 
+public class CultureAffinityTestSuite : BasicTest
 {
     GameObject TestAffinityObj;
     AffinityManager TestAffinityManager;
@@ -20,57 +20,45 @@ public class CultureAffinityTestSuite
         TestAffinityManager.XDisplacement = 2;
         TestAffinityManager.GrowthRate = .5f;
         TestAffinityManager.YDisplacement = 1f;
-        TestAffinityManager.DecaySteepness = .1f;
         TestAffinityManager.Initialize();
         yield return null;
     }
 
-    // TODO: need to tweak the forgetting rate
 
     [Test]
     public void CanGainAffinityOnBiome()
     {
-        HarvestForDays(100000);
-        Assert.AreEqual(4.676436f, TestAffinityManager.GetAffinity(TileDrawer.BiomeType.Grassland), "Affinity did not gain at expected rate!");
+        HarvestForDays(5, TileDrawer.BiomeType.Grassland);
+        Assert.AreEqual(TestUtils.ThreeDecimals(3.08303f), TestUtils.ThreeDecimals(TestAffinityManager.GetAffinity(TileDrawer.BiomeType.Grassland)), "Affinity did not gain at expected rate!");
         foreach (TileDrawer.BiomeType biome in Enum.GetValues(typeof(TileDrawer.BiomeType)))
         {
-            if(biome != TileDrawer.BiomeType.Grassland) Assert.That(TestAffinityManager.GetAffinity(biome)! == 1, $"Affinity should be 1, but {biome} is at {TestAffinityManager.GetAffinity(biome)}!");
+            if(biome != TileDrawer.BiomeType.Grassland) Assert.That(TestAffinityManager.GetAffinity(biome) == 0, $"Affinity should be 0, but {biome} is at {TestAffinityManager.GetAffinity(biome)}!");
         }
     }
 
     [Test]
     public void CanDecayAffinityOnBiome()
     {
-        HarvestForDays(1000);
-        DecayForDays(100);
-        Assert.AreEqual(0, TestAffinityManager.GetAffinity(TileDrawer.BiomeType.Grassland), "Affinity did not gain at expected rate!");
+        HarvestForDays(30, TileDrawer.BiomeType.Grassland);
+        HarvestForDays(1, TileDrawer.BiomeType.Savannah);
+        HarvestForDays(7, TileDrawer.BiomeType.Grassland);
 
-        foreach (TileDrawer.BiomeType biome in Enum.GetValues(typeof(TileDrawer.BiomeType)))
-        {
-            Assert.That(TestAffinityManager.GetAffinity(biome)! < 0, $"Affinity should never be less than zero, but {biome} is at {TestAffinityManager.GetAffinity(biome)}!");
-        }
-    
+        Assert.AreEqual(TestUtils.ThreeDecimals(7.018f), TestUtils.ThreeDecimals(TestAffinityManager.GetAffinity(TileDrawer.BiomeType.Grassland)), "Affinity did not decay at expected rate!");
     }
 
  
 
-    void HarvestForDays(int numDays)
+    void HarvestForDays(int numDays, TileDrawer.BiomeType biome)
     {
         foreach(var _ in Enumerable.Range(0, numDays))
         {
-            //Debug.Log("Harvesting. Affinity is now " + TestAffinityManager.GetAffinity(TileDrawer.BiomeType.Grassland));
-            TestAffinityManager.HarvestedOnBiome(TileDrawer.BiomeType.Grassland);
+            TestAffinityManager.HarvestedOnBiome(biome);
+            EventManager.TriggerEvent("Tick", null);
+            //Debug.Log("Harvesting. Affinity is now " + TestAffinityManager.GetAffinity(biome));
         }
+        Debug.Log($"Final affinity for {biome} is {TestAffinityManager.GetAffinity(biome)}");
     }
 
-    void DecayForDays(int numDays)
-    {
-        foreach (var _ in Enumerable.Range(0, numDays))
-        {
-            Debug.Log("Decaying. Affinity is now " + TestAffinityManager.GetAffinity(TileDrawer.BiomeType.Grassland));
-            TestAffinityManager.DecayAffinities(TileDrawer.BiomeType.Barren);
-        }
-    }
 
     
 
