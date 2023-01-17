849a7237b03426f56c1ce4119a205a9fedf73147
Steve <wenzelstev@gmail.com>
2023-01-17 10:49:32 -0500

    experimental work on changing cultureactions to structs

--- Assets/Resources/Scripts/CultureScripts/Actions/CultureAction.cs
+++ Assets/Resources/Scripts/CultureScripts/Actions/CultureAction.cs
@@ -2,22 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class CultureAction
+public interface ICultureAction
 {
-    public Culture Culture { get; private set; }
-    public Turn turn;
+    public Culture Culture { get; }
+    float ActionCost { get; }
 
-    protected float ActionCost = 1; // cost per pop for this culture to perform this action
-
-    protected CultureAction(Culture c)
-    {
-        //Debug.Log("starting turn for " + c.GetHashCode());
-        Culture = c;
-        turn = Turn.CurrentTurn;
-        //Debug.Log($"{culture.Population}, {ActionCost}");
-        Turn.AddUpdate(CultureUpdateGetter.GetFoodUpdate(this, Culture, -ActionCost * Culture.Population)); // doing it here means you lose additional food per turn if you call more than one action per turn
-        //Debug.Log(turn.UpdateCulture(culture).FoodChange);
-    }
 
     /// <summary>
     /// Get the cost of this action. Returns a negative value.
